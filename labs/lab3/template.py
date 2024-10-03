cf85799b9bbe7815a7ee9e28ae7fdc9267009c96
sob05001 <solomon.berhe@gmail.com>
2024-10-03 14:17:14 -0700

    Update and rename template_generate_1000_mongodb_docs.py to template.py

--- labs/lab3/template.py
+++ labs/lab3/template.py
@@ -1,6 +1,8 @@
 # pip install Flask-PyMongo
 from flask import Flask
 from flask_pymongo import PyMongo
+from bson import json_util, ObjectId
+import json
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://i40:dbms2@cluster0.lixbqmp.mongodb.net/lab3"
@@ -13,3 +15,12 @@ for i in range(1000):
     o = {"uuid":i, "source":"mongodb", "creationTimestamp":12345, "updatedTimestamp":12345, "A":"A", "B":"B", "C":"C"}
     print(o)
     mongo.db.test2.insert_one(o)
+
+@app.route("/mongodb")
+def mongodb():
+    data = mongo.db.test2.find({})
+    page_sanitized = json.loads(json_util.dumps(data))
+    return page_sanitized
+
+if __name__ == '__main__':  
+   app.run()
