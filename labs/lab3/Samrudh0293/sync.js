91137fdf2abd8d65b1959e2ad4d76fc40636eb28
Samrudh Uchil <rtrsamrudhuchil@gmail.com>
2024-10-08 02:02:26 -0700

    Update sync.js

--- labs/lab3/Samrudh0293/sync.js
+++ labs/lab3/Samrudh0293/sync.js
@@ -18,14 +18,12 @@ async function syncDataToMongoDB(data) {
         const collection = db.collection(collectionName);
 
         const syncPromises = data.map(async (object) => {
-            const { uuid, _id, ...updateObject } = object; // Exclude _id if it exists in object
+            const { uuid, _id, ...updateObject } = object;
             const existingRecord = await collection.findOne({ uuid: object.uuid });
 
             if (existingRecord) {
-                // Only update fields other than _id
                 await collection.updateOne({ uuid: object.uuid }, { $set: updateObject });
             } else {
-                // Insert new record
                 await collection.insertOne(object);
             }
         });
